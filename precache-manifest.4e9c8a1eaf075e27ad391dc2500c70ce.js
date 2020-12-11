a23f2d5a2ce7ce87599bc8444aa70d1062c0514c
Клевакин Артем Николаевич <klevakin@tochka.com>
2020-12-11 13:43:57 +0500

    Updates

---
+++ precache-manifest.4e9c8a1eaf075e27ad391dc2500c70ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7435e4c3afadb49e1afea58b02f2d42e",
+    "url": "/js-advanced/index.html"
+  },
+  {
+    "revision": "1d04c54f92468ad19948",
+    "url": "/js-advanced/static/css/main.33921b11.chunk.css"
+  },
+  {
+    "revision": "264f1f927211a7bf2496",
+    "url": "/js-advanced/static/js/2.5eddb488.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/js-advanced/static/js/2.5eddb488.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d04c54f92468ad19948",
+    "url": "/js-advanced/static/js/main.28fa14aa.chunk.js"
+  },
+  {
+    "revision": "66e5fbcd9110e0ab518a",
+    "url": "/js-advanced/static/js/runtime-main.f39c4875.js"
+  }
+]);
