a1f4077c2976ea16c6a7ebde8a2825ebaaec6757
Клевакин Артем Николаевич <themightymasha@gmail.com>
2021-01-30 17:38:54 +0500

    Lesson 3, layout

--- src/lessons/lesson3/Lesson3.tsx
+++ src/lessons/lesson3/Lesson3.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import API from './API';
 import './lesson_3';
+import s from './Lesson3.module.css';
 
 const Lesson3 = () => {
   const [searchName, setSearchName] = useState('');
@@ -13,6 +14,7 @@ const Lesson3 = () => {
   let apiFn = (fn: any) => {
     return fn.then((response: any) => {
       if (response.Search) {
+        console.log(response.Search);
         setSearchResult(response.Search);
       }
     });
@@ -29,28 +31,46 @@ const Lesson3 = () => {
 
   return (
     <div>
-      <h1>Promises</h1>
-      <div>
-        <h3><p>Search by name:</p></h3>
-        <input type="text" value={searchName} onChange={(e) => setSearchName(e.currentTarget.value)} />
-        <button onClick={searchFilm}>Search</button>
-        <div>
-          {
-            searchResult.map((item: any, index: number) => <div key={index}>{item.Title}</div>)
-          }
-        </div>
-      </div>
+      {/*<h1>Promises</h1>*/}
+      <div className={s.movies}>
 
-      <div>
-        <h3><p>Search by type:</p></h3>
-        <input type="text" value={searchNameByType} onChange={(e) => setSearchNameByType(e.currentTarget.value)} />
-        <button onClick={searchByType} data-t='movie'>Movie</button>
-        <button onClick={searchByType} data-t='series'>Series</button>
+        <div className={s.movies_search}>
+          <h3><p>Search by name:</p></h3>
+          <input type="text" value={searchName} onChange={(e) => setSearchName(e.currentTarget.value)} />
+          <button className={s.button} onClick={searchFilm}>Search</button>
+
+          <h3><p>Search by type:</p></h3>
+          <input type="text" value={searchNameByType} onChange={(e) => setSearchNameByType(e.currentTarget.value)} />
+          <button className={s.button} onClick={searchByType} data-t='movie'>Movie</button>
+          <button className={s.button} onClick={searchByType} data-t='series'>Series</button>
+        </div>
+
+        <div>
+          <ul className={s.movies_list}>
+
+            {
+              searchResult.map((item: any, index: number) =>
+                <li className={s.movie_item} key={index}>
+                  <div className={s.movie_poster}>
+                    <img src={item.Poster} alt={item.Title} width={250} height={350} />
+                  </div>
+
+                  <div className={s.movie_info}>
+                    <p>{item.Title}</p>
+                    <p>{item.Type}</p>
+                    <p>{item.Year}</p>
+                  </div>
+                </li>)
+            }
+          </ul>
+        </div>
+
         <div>
           {
             searchResultByType.map((item: any, index: number) => <div key={index}>{item.Title}</div>)
           }
         </div>
+
       </div>
     </div>
   );
